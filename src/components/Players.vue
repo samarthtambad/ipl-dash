5acb8e8b8bc9ab8a0e9353d27be7a19ba1631c69
Samarth Tambad <samarth@moonraft.com>
2017-11-14 19:08:52 +0530

    Included Bulma, added header

---
+++ src/components/Players.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="players">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Players',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

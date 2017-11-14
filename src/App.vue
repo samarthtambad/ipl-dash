4e8adb6a86fd493b864380493b831807a4f58214
Samarth Tambad <tambad.samarth@gmail.com>
2017-11-14 19:08:52 +0530

    Included Bulma, added header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <img src="./assets/logo.png"> -->
+    <header class="navbar is-dark" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <h1 class="navbar-item">IPL Dashboard</h1>
+          <button class="button navbar-burger is-dark">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-end">
+            <router-link class="navbar-item" to="/">Home</router-link>
+            <router-link class="navbar-item" to="/teams">Teams</router-link>
+            <router-link class="navbar-item" to="/players">Players</router-link>
+            <router-link class="navbar-item" to="/facts">Fun Facts</router-link>
+          </div>
+        </div>
+      </div>
+    </header>
+    <router-view class="view"/>
   </div>
 </template>
 
@@ -12,12 +32,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <div class="section">
+        <div class="container">
+            <div class="tile is-ancestor">
+                <div id="team-wins-donut" class="tile is-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
+                <div class="tile"></div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

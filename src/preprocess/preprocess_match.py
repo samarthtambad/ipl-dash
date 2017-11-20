23d351be2e0b8f1f68be53afe77343d4ac089a5f
Samarth Tambad <tambad.samarth@gmail.com>
2017-11-20 20:36:11 +0530

    JSON Data ready

--- src/preprocess/preprocess_match.py
+++ src/preprocess/preprocess_match.py
@@ -1,42 +1,30 @@
 import pandas as pd # data processing
 import json
+from collections import defaultdict
 
-balls_df = pd.read_csv('../assets/data/processed/ball_by_ball__modified.csv')
-matches_df = pd.read_csv('../assets/data/processed/matches_modified.csv')
 
-# season = matches_df.groupby('season')
-# print season
+temp = pd.read_csv('../assets/data/match_team_mod.csv')
 
-# for i in xrange(1,10):
-#     season = matches_df[matches_df['season'] == i]
-#     team_1 = season['team_1']
-#     team_1['val'] = 1
-#     atotal = team_1.groupby('team_1').sum()
-#     team_2 = season['team_2']
-#     team_2['val'] = 1
-#     btotal = team_2.groupby('team_2').sum()
 
-df = matches_df
-del df['date']
-del df['venue']
-del df['is_superover']
-del df['is_duckworthlewis']
-del df['first_umpire']
-del df['second_empire']
-del df['city']
-del df['host_country']
-df.set_index('match')
-# print df.head(13)
+# results = defaultdict(lambda: defaultdict(dict))
+# grouped = temp.groupby('season').count()
 
-matches = df['team_1'].groupby(df['season'])
-df2 =  matches.count().reset_index(name = 'count')
-print df2
+# for t in grouped.itertuples():
+#     for i, key in enumerate(t.Index):
+#         if i == 0:
+#             nested = results[key]
+#         elif i == len(index) - 1:
+#             nested[key] = value
+#         else:
+#             nested = nested[key]
 
-df1 = df[df['season']==1]
-# print df1
 
-df1 = df.groupby(['season','team_1']).count()
 
-# df = df1.iloc[:,0:1]
-# print df.head()
-# df1 = df[['season', 'team_1', 'match']]
+
+# temp1 = temp.groupby('season').apply(lambda x: x.to_json("batsmen.json", orient='records'))
+# print temp.groupby('season').apply(lambda x: x.to_json(orient='records'))
+
+# temp1 = temp.groupby('season').apply()
+temp.to_json('match_team.json', orient='records')
+# with open('batsmen.json', 'w') as f:
+#     f.write(str(temp1))
